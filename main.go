b58672735a8ec729697a06ef2b617e4c01cae822
Michael Kuzmin <michael.kuzmin@jetbrains.com>
2022-10-27 16:10:35 +0300

    debug option

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"context"
+	"flag"
+	"log"
 	"terraform-provider-teamcity/teamcity"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 )
 
 func main() {
-	providerserver.Serve(context.Background(), teamcity.New, providerserver.ServeOpts{
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	err := providerserver.Serve(context.Background(), teamcity.New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/jetbrains/teamcity",
+		Debug:   debug,
 	})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
