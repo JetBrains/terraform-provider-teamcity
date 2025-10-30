3503f4e3a2a9b7f369878bed0dfda4387b457da9
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2025-10-30 11:44:56 +0200

    feat(tests) TCCI-4919 Add project acc tests

--- teamcity/project_test.go
+++ teamcity/project_test.go
@@ -1,8 +1,9 @@
 package teamcity
 
 import (
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
 func TestAccProject_basic(t *testing.T) {
@@ -41,6 +42,19 @@ resource "teamcity_project" "test" {
 					resource.TestCheckResourceAttr("teamcity_project.test", "id", "new"),
 				),
 			},
+			// Import the project and verify state
+			{
+				Config: providerConfig + `
+resource "teamcity_project" "test" {
+	name = "test2"
+	id   = "new"
+}
+`,
+				ResourceName:      "teamcity_project.test",
+				ImportState:       true,
+				ImportStateId:     "new",
+				ImportStateVerify: true,
+			},
 
 			//TW-88034
 			{
@@ -63,6 +77,52 @@ resource "teamcity_project" "child" {
 				),
 			},
 			//TW-88034 ^
+
+			// Re-parent child from parent -> parent2 and verify
+			{
+				Config: providerConfig + `
+resource "teamcity_project" "parent" {
+	name = "parent"
+	id = "parent_project"
+}
+
+resource "teamcity_project" "parent2" {
+	name = "parent2"
+	id = "parent_project2"
+}
+
+resource "teamcity_project" "child" {
+	name = "child"
+	id = "child_project"
+	parent_project_id = teamcity_project.parent2.id
+}
+`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("teamcity_project.child", "id", "child_project"),
+					resource.TestCheckResourceAttr("teamcity_project.child", "parent_project_id", "parent_project2"),
+					resource.TestCheckResourceAttr("teamcity_project.parent", "parent_project_id", "_Root"),
+					resource.TestCheckResourceAttr("teamcity_project.parent2", "parent_project_id", "_Root"),
+				),
+			},
+			// Import the child project and verify state
+			{
+				Config: providerConfig + `
+resource "teamcity_project" "parent2" {
+	name = "parent2"
+	id = "parent_project2"
+}
+
+resource "teamcity_project" "child" {
+	name = "child"
+	id = "child_project"
+	parent_project_id = teamcity_project.parent2.id
+}
+`,
+				ResourceName:      "teamcity_project.child",
+				ImportState:       true,
+				ImportStateId:     "child_project",
+				ImportStateVerify: true,
+			},
 		},
 	})
 }
